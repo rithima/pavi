bd7f2c0b4334894d26ee22946bc9de2cf904dbf5
rithima <32138601+rithima@users.noreply.github.com>
2018-01-21 12:10:13 +0530

    Create array N.c

---
+++ array N.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int i,n,k,b=0;
+	scanf("%d%d",&n,&k);
+	inta[5];
+	for(i=1;i<=k;i++)
+	{
+		printf("%d",&a[i]);
+	}
+	for(i=1;i<=k;i++)
+	{
+		b=b+a[i];
+	}
+	printf("sum is %d",b);
+	return 0;
+}
