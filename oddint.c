cefe6ef8dd110eff2ee6e81c374811a9445ed110
rithima <32138601+rithima@users.noreply.github.com>
2018-01-22 21:27:34 +0530

    Create oddint.c

---
+++ oddint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+	int i,f,1;
+	printf("enter first intervel");
+	scanf("%d",&f);
+	pritf("enter second intevel");
+	scanf("%",&1);
+	{
+		if(i%2==1)
+		{
+			printf("%d is an odd no",i);
+		}
+	}
+	return 0;
+}
