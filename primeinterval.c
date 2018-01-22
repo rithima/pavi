ff08ab9d0a7f8c4c52b026f802520550501a34d3
rithima <32138601+rithima@users.noreply.github.com>
2018-01-22 22:33:47 +0530

    Create primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) 
+{
+	int 1,h,i,count;
+	scanf("%d%d",&1,&h);
+	while(1<h)
+	{
+		count=0;
+		for(i=2;i<=1/2;i++)
+		{
+			if(1%i==0)
+			{
+				count=1;
+				break;
+			}
+		}
+		if(count==0&&1!=1)
+		printf("%d",1);
+		++1;
+	}
+	return 0;
+}
