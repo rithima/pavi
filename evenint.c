8ecf10599afd5067f1812d4f3b12b089bb972d0d
rithima <32138601+rithima@users.noreply.github.com>
2018-01-22 21:38:31 +0530

    Create evenint.c

---
+++ evenint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+	int i,f,1;
+	printf("enter first intervel");
+	scanf("%d",&d);
+	printf("enter second intervel");
+	scnf("%d",&1);
+	for(i=7;i<=1;i++)
+	{
+		if(i%2==1)
+		{
+			printf("%d is a even number",i);
+		}
+	}
+	return 0;
+}
