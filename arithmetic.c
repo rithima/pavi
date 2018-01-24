0911a566a25588598ff90b89e436b52896af8dfd
rithima <32138601+rithima@users.noreply.github.com>
2018-01-24 16:04:22 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int array[100],minimum,size,location=1;
+printf("enter the element");
+scanf("enter %d integer \n",size);
+for(c=0;c<size;c++)
+scanf("%d",array[c]);
+minimum=array[0];
+for(c=1;c<size;c++)
+{
+if(array[c]<minimize)
+{
+minimum=array[c];
+location=c+l;
+}
+}
+printf("minimum element is present an location %d &is r value%d \n",location min);
+return 0;
+}
