88115416042379741fb3cd5e0b6c503b9725f4f0
rithima <32138601+rithima@users.noreply.github.com>
2018-03-02 16:48:06 +0530

    Create numeric char 35.c

---
+++ numeric char 35.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main (void)
+{
+	int i,c=0;
+	char a[50];
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>='0'&&a[i]<='9')
+		{
+			c++;
+		}
+	}
+	printf("%d",c);
+	return 0;
+}
