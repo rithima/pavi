3a32ebb3b99de00dbe50d1ac4c0d30b031ad2eb5
rithima <32138601+rithima@users.noreply.github.com>
2018-03-16 16:16:48 +0530

    Create program41.c

---
+++ program41.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char s[10];
+	int i,k;
+	printf("enter the string:");
+	scanf("%s",s);
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	{
+	
+		printf("%s\n",s);
+	}
+	return 0;
+	
+}
