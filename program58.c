58ac87a6807169fa97e9d6f5f8047e1b10e90f56
rithima <32138601+rithima@users.noreply.github.com>
2018-03-20 09:32:14 -0700

    Create program58.c

---
+++ program58.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("value of a and b before swapping %d %d \n",a,b);
+            a=a^b;
+            b=a^b;
+            a=a^b;
+            printf("value of a and b after swapping %d %d \n",a,b);
+	return 0;
+}
