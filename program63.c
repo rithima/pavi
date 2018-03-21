393597db86a3b6a60c71f1ae6a58a5d8e48d1227
rithima <32138601+rithima@users.noreply.github.com>
2018-03-21 09:20:03 -0700

    Create program63.c

---
+++ program63.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	int i,count=1;
+	char a[50];
+	gets(a);
+		for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i] == ' ')
+		{
+		count++;
+		}
+		
+	}
+	printf("%d",count);
+	return 0;
+}
+		
